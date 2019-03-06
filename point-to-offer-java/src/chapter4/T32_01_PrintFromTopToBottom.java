658365622bc7ca76e51fa205221127ae8560b048
jaywongs <wangshijiejay@gmail.com>
2019-03-06 08:59:21 +0800

    auto commit

---
+++ point-to-offer-java/src/chapter4/T32_01_PrintFromTopToBottom.java
@@ -0,0 +1,7 @@
+package chapter4;
+
+/**
+ * Created by jaywangs on 2019/3/5
+ */
+public class T32_01_PrintFromTopToBottom {
+}
---
+++ point-to-offer-java/src/chapter4/T32_03_PrintTreeInZigzag.java
@@ -0,0 +1,33 @@
+package chapter4;
+
+import structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by jaywangs on 2019/3/5
+ */
+public class T32_02_PrintFromTopToBottom {
+    public ArrayList<ArrayList<Integer>> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int cnt = queue.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            while (cnt-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            if (list.size() != 0)
+                res.add(list);
+        }
+        return res;
+    }
+}
