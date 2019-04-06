6ddc69d54cb48759cb294b7048b2fb339ee9c329
jaywongs <wangshijiejay@gmail.com>
2019-04-06 23:57:08 +0800

    auto commit

---
+++ LeetCode-java/src/solutions/greedyAlgorithm/T406_QueueReconstructionbyHeight.java
@@ -0,0 +1,7 @@
+package solutions.greedyAlgorithm;
+
+/**
+ * Created by jaywangs on 2019/4/6
+ */
+public class T406_QueueReconstructionbyHeight {
+}
---
+++ Others/src/tencent/Main2.java
@@ -0,0 +1,7 @@
+package tencent;
+
+/**
+ * Created by jaywangs on 2019/4/5
+ */
+public class Main2 {
+}
